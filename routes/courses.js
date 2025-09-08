a9232742ad95dc88c9e9e16988262f11fb48ad7f
= <=>
2025-09-08 19:27:36 +0530

    updated

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,9 @@
 // backend/routes/courses.js
 const express = require('express');
 const router = express.Router();
-const {getAllCourses,createCourse,getCourseById} = require('../controllers/CourseController');
+const {getAllCourses,createCourse} = require('../controllers/CourseController');
 
 router.get('/getAllCourse', getAllCourses);
-router.get('/:id', getCourseById);
 router.post('/addCourse', createCourse);
 
 module.exports = router;
