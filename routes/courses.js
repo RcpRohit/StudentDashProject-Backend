b3f260b7c5418e98ff89b32fc974257a5ff55ad3
RcpRohit <168912119+RcpRohit@users.noreply.github.com>
2025-09-08 19:18:31 +0530

    Update courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,5 @@ const router = express.Router();
 const {getAllCourses,createCourse,getCourseById} = require('../controllers/CourseController');
 
 router.get('/getAllCourse', getAllCourses);
-router.get('/:id', getCourseById);
 router.post('/addCourse', createCourse);
-
 module.exports = router;
